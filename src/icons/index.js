1048967a99754269f328f28f952cabc1f5b0fd7a
shanzhaozhen <204447118@qq.com>
2020-04-02 17:42:24 +0800

    更新一些说明

--- src/icons/index.js
+++ src/icons/index.js
@@ -7,4 +7,11 @@ Vue.component('svg-icon', SvgIcon)
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 export const allIcons = req.keys().map(item => item.match(/\.\/(\S*)\.svg/)[1])
+
+const re = /\.\/(.*)\.svg/
+
+export const svgIcons = requireAll(req).map(i => {
+  return i.match(re)[1]
+})
+
 requireAll(req)
